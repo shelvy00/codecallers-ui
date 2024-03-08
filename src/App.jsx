008a847db10a1a45218b17920458a2d3c321a050
Dean Wilson <Dean Wilson>
2024-03-07 20:07:42 -0600

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Hello from "./components/Hello";
+
+function App() {
+
+  return (
+   <>
+      <div>
+	      < Hello />
+      </div>
+   </>
+  )
+}
+
+export default App
