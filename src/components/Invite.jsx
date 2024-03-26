e95834e96d1db1ad839bcb4928f1908b281e1c99
ngarner98 <exmachina091098@gmail.com>
2024-03-25 20:06:31 -0500

    Added invite box to invite page

--- src/components/Invite.jsx
+++ src/components/Invite.jsx
@@ -1,11 +1,46 @@
 import React, { useEffect, useState } from 'react';
+import {Box, TextField, Stack, Button} from '@mui/material';
 
 function Invite() {
+
+  const[inviteEmail, setInviteEmail]=React.useState('')
+  const[message, setMessage]=React.useState('')
+  
   
   return (
-    <div>
-      <h2>Invite</h2>
-    </div>
+    <Box
+    component="form"
+    sx={{
+      '& > :not(style)': { m: 7, width: '40ch' },
+    }}
+    noValidate
+    autoComplete="off"
+    style={{border: '5px solid rgba(, 0, 0, 0.96)', padding: '50px', borderRadius: '25px'}}>
+      <div>
+        <h2>Invite</h2>
+      </div>
+      <div>
+          <TextField id="inviteEmail" label="Email" variant="outlined" 
+            value={inviteEmail}
+            onChange={(event)=>setInviteEmail(event.target.value)} 
+            required
+          />
+      </div>
+      <div>
+          <TextField
+            id="message"
+            label="Message"
+            multiline
+            rows={5}
+            value={message}
+            onChange={(event)=>setMessage(event.target.value)} 
+            required
+          />
+      </div>
+      <div>
+        <Button variant="outlined">Send Invite</Button> 
+      </div>
+    </Box>
   );
 }
 
