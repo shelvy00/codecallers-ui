53d0aed603e3c66285405bdeeced7a30cec2fdce
ngarner98 <exmachina091098@gmail.com>
2024-03-28 00:35:08 -0500

    Added button mapping for invite functionality, getting CORS error

--- src/components/Invite.jsx
+++ src/components/Invite.jsx
@@ -3,8 +3,43 @@ import {Box, TextField, Stack, Button} from '@mui/material';
 
 function Invite() {
 
-  const[inviteEmail, setInviteEmail]=React.useState('')
-  const[message, setMessage]=React.useState('')
+  const[inviteEmail, setInviteEmail]=React.useState('');
+  const[message, setMessage]=React.useState('Come join and check out this website!' + '\n[INSERT URL HERE]');
+
+
+  const handleClick = (event) => {
+    event.preventDefault()
+    const invite = { inviteEmail, message };
+    if (inviteEmail === '' || message === '') {
+      alert("All fields are required!");
+      event.preventDefault();
+    } else if (message.length < 1) {
+      alert("Please provide an invite message!");
+      event.preventDefault();
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(inviteEmail)) {
+      alert("Invalid email address!");
+      event.preventDefault();
+    }
+    else
+      fetch("http://localhost:8080/Invite", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(invite)
+      })
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          alert("Invite sent!");
+          window.location.href = '/invite';
+        })
+        .catch(error => {
+          console.error('There was a problem with the fetch operation:', error);
+          alert("Error occurred while submitting the form. Please try again later.");
+        });
+  }
   
   
   return (
@@ -33,14 +68,16 @@ function Invite() {
             label="Message"
             multiline
             rows={5}
+            
             value={message}
             fullWidth
             onChange={(event)=>setMessage(event.target.value)} 
-            required
           />
       </div>
       <div>
-        <Button variant="outlined">Send Invite</Button> 
+        <Button variant= "contained" onClick={handleClick}>
+          Send Invite
+        </Button> 
       </div>
     </Box>
   );
